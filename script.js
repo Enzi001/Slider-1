ee45bf0e06fdd2893bfc84b1e326fdd1996ad863
Enzi001 <129244493+Enzi001@users.noreply.github.com>
2023-11-13 17:04:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const rotateBtn = document.querySelector(".rotate-btn");
+const slides = document.querySelectorAll(".bg-slide");
+const TotalSlides = slides.length;
+let index = 0;
+
+rotateBtn.addEventListener("click", () => {
+    
+    slides.forEach(slide=>{
+        if(slide.classList.contains('active')){
+            slide.classList.add("after-active")
+        }
+        else{
+            slide.classList.remove("after-active")
+        }
+    })
+
+    slides[index].classList.remove("active");
+
+  index++;
+
+  if (index == TotalSlides) {
+    index = 0;
+  }
+
+  slides[index].classList.add("active");
+});
